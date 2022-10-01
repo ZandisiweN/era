f2e17c7295c67b7c9bb4db6f924227232ebe4db0
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2022-10-02 00:00:10 +0200

    Creat node server

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Xiomi Mi Bedside Lamp 2',
+      slug: 'xiomi-bedside-lamp',
+      category: 'Lamps',
+      image: '../images/basket.jfif',
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Fill your room with natural light with the Xiaomi Mi Bedside Lamp 2! Convenient Wi-Fi and Bluetooth LE connectivity combined with voice control ensures comfort and ease of control of the lamp.',
+    },
+    {
+      name: 'Light Beige Ceramic Vase',
+      slug: 'ceramic-vase',
+      category: 'Vases',
+      image: '../images/basket.jfif',
+      price: 209,
+      countInStock: 50,
+      rating: 4.0,
+      numReviews: 10,
+      description:
+        'Round pottery vase. Diameter 20 cm, height 21 cm, hole diameter 3.5 cm.',
+    },
+    {
+      name: 'Marble soap dispenser',
+      slug: 'marble-soap-dispenser',
+      category: 'Bathroom Accessories',
+      image: '../images/basket.jfif',
+      price: 89,
+      countInStock: 45,
+      rating: 3.0,
+      numReviews: 13,
+      description:
+        'Marble soap dispenser. Top 100% stainless steel, Upper Material: Stone 100%. Dimensions 7x7x18.5 cm. Marble is a natural material, therefore individual pieces may differ in color and structure..',
+    },
+    {
+      name: 'Red Smeg Kettle',
+      slug: 'smeg-kettle',
+      category: 'Bathroom Accessories',
+      image: '../images/basket.jfif',
+      price: 89,
+      countInStock: 45,
+      rating: 3.0,
+      numReviews: 13,
+      description:
+        'Marble soap dispenser. Top 100% stainless steel, Upper Material: Stone 100%. Dimensions 7x7x18.5 cm. Marble is a natural material, therefore individual pieces may differ in color and structure..',
+    },
+  ],
+}
+
+export default data
