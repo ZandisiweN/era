806c0c78b113b48c5eb4034622ac2f796a5c498c
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2022-10-01 23:21:20 +0200

    Added Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import CategoryScreen from './screens/CategoryScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductsScreen from './screens/ProductsScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">E R A</a>
-      </header>
-      <main>List Products</main>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/products" element={<ProductsScreen />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
