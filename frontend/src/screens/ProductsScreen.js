ae0b31f152dfaffaa5be30bf04423cfe7907aadd
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2022-10-02 10:49:26 +0200

    Manage State by Reducer Hook

--- frontend/src/screens/ProductsScreen.js
+++ frontend/src/screens/ProductsScreen.js
@@ -1,29 +1,72 @@
-import React from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+import axios from 'axios'
+import logger from 'use-reducer-logger'
+// import data from '../data'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true }
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false }
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
 
 const ProductsScreen = () => {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  })
+  // const [products, setProducts] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get('/api/products')
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message })
+      }
+
+      // setProducts(result.data)
+    }
+    fetchData()
+  }, [])
+  console.log(products)
   return (
     <div>
       <h1>Trending Products</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products &&
+          products.map((product) => (
+            <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>ADD TO CART</button>
+
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>ADD TO CART</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   )
