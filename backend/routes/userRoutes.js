2d0e86fc65b53b511e5093d5d05bb162db75a538
Zandisiwe Ndhlovu <ndhlovuzandisiwe@gmail.com>
2022-11-17 00:49:07 +0100

    Rate and Review Products

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,26 +16,6 @@ userRouter.get(
   })
 )
 
-userRouter.post(
-  '/signin',
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email })
-    if (user) {
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        res.send({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          token: generateToken(user),
-        })
-        return
-      }
-    }
-    res.status(401).send({ message: 'Invalid email or password' })
-  })
-)
-
 userRouter.get(
   '/:id',
   isAuth,
@@ -65,6 +45,45 @@ userRouter.put(
     } else {
       res.status(404).send({ message: 'User Not Found' })
     }
+  })
+)
+
+userRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    if (user) {
+      if (user.email === 'admin@example.com') {
+        res.status(400).send({ message: 'Can Not Delete Admin User' })
+        return
+      }
+      await user.remove()
+      res.send({ message: 'User Deleted' })
+    } else {
+      res.status(404).send({ message: 'User Not Found' })
+    }
+  })
+)
+
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        })
+        return
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' })
   })
 )
 
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useReducer } from 'react'
 import Button from 'react-bootstrap/Button'
 import { Helmet } from 'react-helmet-async'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import { Store } from '../Store'
@@ -16,6 +17,14 @@ const reducer = (state, action) => {
       return { ...state, users: action.payload, loading: false }
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload }
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true, successDelete: false }
+    case 'DELETE_SUCCESS':
+      return { ...state, loadingDelete: false, successDelete: true }
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false }
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false }
     default:
       return state
   }
@@ -23,10 +32,11 @@ const reducer = (state, action) => {
 
 export const UserListScreen = () => {
   const navigate = useNavigate()
-  const [{ loading, error, users }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: '',
-  })
+  const [{ loading, error, users, loadingDelete, successDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: '',
+    })
 
   const { state } = useContext(Store)
   const { userInfo } = state
@@ -41,11 +51,34 @@ export const UserListScreen = () => {
 
         dispatch({ type: 'FETCH_SUCCESS', payload: data })
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err),
+        })
       }
     }
-    fetchData()
-  }, [userInfo])
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' })
+    } else {
+      fetchData()
+    }
+  }, [userInfo, successDelete])
+
+  const deleteHandler = async (user) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      try {
+        dispatch({ type: 'DELETE_REQUEST' })
+        await axios.delete(`/api/users/${user._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        })
+        toast.success('user deleted successfully')
+        dispatch({ type: 'DELETE_SUCCESS' })
+      } catch (error) {
+        toast.error(getError(error))
+        dispatch({ type: 'DELETE_FAIL' })
+      }
+    }
+  }
 
   return (
     <div>
@@ -53,6 +86,8 @@ export const UserListScreen = () => {
         <title>Users</title>
       </Helmet>
       <h1>Users</h1>
+
+      {loadingDelete && <LoadingBox></LoadingBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
@@ -82,6 +117,14 @@ export const UserListScreen = () => {
                     onClick={() => navigate(`/admin/user/${user._id}`)}
                   >
                     Edit
+                  </Button>
+                  &nbsp;
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => deleteHandler(user)}
+                  >
+                    Delete
                   </Button>
                 </td>
               </tr>
